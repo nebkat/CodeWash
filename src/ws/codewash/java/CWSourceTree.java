f15df616bce9e3d024526db53a13d26918dc4796
DeBattlinBeen <cjbeenham@gmail.com>
2019-02-09 23:14:09 +0000

    Java: Added getter/setter to CWSourceTree for packages

--- src/ws/codewash/java/CWSourceTree.java
+++ src/ws/codewash/java/CWSourceTree.java
@@ -5,4 +5,12 @@ import java.util.Map;
 
 public class CWSourceTree {
     private Map<String, CWPackage> mPackages = new HashMap<>();
+
+    public void setPackages(Map<String, CWPackage> packages) {
+        mPackages = packages;
+    }
+
+    public Map<String, CWPackage> getPackages() {
+        return mPackages;
+    }
 }
