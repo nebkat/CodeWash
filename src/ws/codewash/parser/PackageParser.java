f7430481998aea08f338362cb3166b77e69c17db
DeBattlinBeen <cjbeenham@gmail.com>
2019-02-11 23:41:18 +0000

    Parser: Fixed PackageParser; more efficient

--- src/ws/codewash/parser/PackageParser.java
+++ src/ws/codewash/parser/PackageParser.java
@@ -5,45 +5,43 @@ import ws.codewash.reader.Source;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class PackageParser extends Parser {
     public Map<String, CWPackage> parsePackages(Map<Source, String> sources) {
         Map<String, CWPackage> packages = new HashMap<>();
-        boolean found;
 
         for (Source s : sources.keySet()) {
-			found = false;
-			Scanner scanner = new Scanner(sources.get(s));
+        	String source = sources.get(s);
+        	Matcher packageMatcher = mPackagePattern.matcher(source);
 
-            while (scanner.hasNextLine() && !found) {
-                String line = scanner.nextLine();
-                Matcher packageLine = mPackagePattern.matcher(line);
+			CWPackage cwPackage = null;
 
-                if (packageLine.find()) {
-                    String name = packageLine.group(Keywords.PACKAGE);
-                    String packageName = "";
-
-                    for (String pack : name.split("([.])")) {
-                        String parentPack = packageName;
-                        packageName += (packageName.length() == 0) ? pack : "." + pack;
-                        if (!packages.containsKey(packageName)) {
-                            CWPackage cwPackage = new CWPackage(packageName);
-                            packages.put(packageName, cwPackage);
-                            if (packages.containsKey(parentPack)) {
-                                CWPackage parent = packages.get(parentPack);
-                                parent.addSubPackage(cwPackage);
-                                cwPackage.setContainer(parent);
-                            }
-                        }
-                    }
-                    found = true;
-                }
-                if (!scanner.hasNextLine()) {
-                    System.err.println("Couldn't parse package from: " + s.getName());
-                }
-            }
+        	while (packageMatcher.find()) {
+        		if (packageMatcher.start() == 0) {
+					if (cwPackage == null) {
+						String name = packageMatcher.group(Keywords.PACKAGE);
+						String packageName = "";
+						for (String pack : name.split("([.])")) {
+							String parentPack = packageName;
+							packageName += (packageName.length() == 0) ? pack : "." + pack;
+							if (!packages.containsKey(packageName)) {
+								cwPackage = new CWPackage(packageName);
+								packages.put(packageName, cwPackage);
+								if (packages.containsKey(parentPack)) {
+									CWPackage parent = packages.get(parentPack);
+									parent.addSubPackage(cwPackage);
+									cwPackage.setContainer(parent);
+								}
+							}
+						}
+					} else {
+						System.err.println("Error Parsing: " + s.getName() + " | More than one package declaration.");
+					}
+				} else {
+					System.err.println("Error Parsing: " + s.getName() + " | Package declaration not at top.");
+				}
+			}
         }
 
         return packages;
