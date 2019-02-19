61215be50f01565822bd85ca8255b8a7f49858b6
DeBattlinBeen <cjbeenham@gmail.com>
2019-02-19 17:31:49 +0000

    main: added zip / jar / folder input streams

--- src/ws/codewash/CodeWash.java
+++ src/ws/codewash/CodeWash.java
@@ -5,10 +5,14 @@ import com.beust.jcommander.ParameterException;
 import ws.codewash.parser.ParsedSourceTree;
 import ws.codewash.parser.Parser;
 import ws.codewash.reader.FolderReader;
+import ws.codewash.reader.SourceReadable;
+import ws.codewash.reader.ZipReader;
 import ws.codewash.util.Arguments;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class CodeWash {
 	public static void main(String[] args) throws IOException {
@@ -20,11 +24,19 @@ public class CodeWash {
 			commander.parse(args);
 			if (Arguments.get().getConfigGenPath() != null) {
 				//Todo: Generate default config
-			}
-			else if (Arguments.get().getSrcPath() != null) {
+			} else if (Arguments.get().getSrcPath() != null) {
 				System.out.println("Washing: " + Arguments.get().getSrcPath());
-				FolderReader folderReader = new FolderReader(Paths.get(Arguments.get().getSrcPath()));
-				ParsedSourceTree parsedSourceTree = new Parser().parse(folderReader.getSources());
+				SourceReadable sources;
+				if (Arguments.get().getSrcPath().endsWith(".zip")) {
+					sources = new ZipReader(Paths.get(Arguments.get().getSrcPath()));
+				} else if (Arguments.get().getSrcPath().endsWith(".jar")) {
+					//Todo: Jar reader
+					sources = () -> null;
+				} else {
+					sources = new FolderReader(Paths.get(Arguments.get().getSrcPath()));
+				}
+				ParsedSourceTree parsedSourceTree = new Parser().parse(sources.getSources());
+
 			} else {
 				//Todo: Create http server
 			}
